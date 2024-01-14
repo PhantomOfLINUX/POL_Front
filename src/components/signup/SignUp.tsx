2f25e9af61d7174b79f17abb6239fb7f6c65710b
KYUTAE PARK <rbxo0109@gmail.com>
2024-01-15 00:37:16 +0900

    feat: 회원가입 목업 완성

--- src/components/signup/SignUp.tsx
+++ src/components/signup/SignUp.tsx
@@ -4,7 +4,7 @@ import React,{useState} from "react"
 
 import SignUpInput from "./SignUpInput"
 
-import { SendAuthentication, CheckAuthentication, checkEmail, CheckName, CheckPassword, CheckPasswordCheck, } from "@/utils/SignUpUtils/SignUpUtil"
+import { SendAuthentication, CheckAuthentication, checkEmail, CheckName, CheckPassword, CheckPasswordCheck, submitSignUp} from "@/utils/SignUpUtils/SignUpUtil"
 
 
 const SignUp = () => {
@@ -15,39 +15,21 @@ const SignUp = () => {
     const [passwordCheck,setPasswordCheck] = useState<string>("");
 
     return (
-        <form>
+        <main>
             <SignUpInput label="이메일" placeholder="name@mail.com" type="email" onChange={setEmail} isVaild={checkEmail(email)} errorMsg="이메일 형식이 다릅니다."/>
             
             <button onClick={(e)=>SendAuthentication(e,email)}>인증번호 발송</button>
 
             <SignUpInput label="인증번호" placeholder="인증번호" type="text" onChange={setEmailCertification} errorMsg=""/>
-            <button onClick={(e)=>CheckAuthentication(email,emailCertification)}>인증번호 제출</button>
+            <button onClick={(e)=>CheckAuthentication(e,email,emailCertification)}>인증번호 제출</button>
 
             <SignUpInput label="닉네임" placeholder="닉네임" type="text" onChange={setName} isVaild={CheckName(name)} errorMsg="닉네임을 써주세요"/>
             <SignUpInput label="비밀번호" placeholder="****" type="password" isVaild={CheckPassword(password)} onChange={setPassword} errorMsg="비밀번호는 숫자, 문자, 기호 8글자 이상입니다."/>
             <SignUpInput label="비밀번호확인" placeholder="****" type="password" isVaild={CheckPasswordCheck(password, passwordCheck)} onChange={setPasswordCheck} errorMsg="비밀번호와 다릅니다"/>
-                        
-            <button>회원가입하기</button>
-        </form>
+
+            <button onClick={(e=>submitSignUp(e,email,emailCertification,name,password,passwordCheck))}>회원가입하기</button>
+        </main>
     )
 }
 
 export default SignUp;
-
-/*
-onSubmit 할 때 체크할 것
-1. 이메일과 이메일 인증번호 확인 
-2. 이름 적혀있는지
-3. 비밀번호 적혀 있고, 비밀번호와 비밀번호 확인이 맞는디
-아래에 빨간 글씨로 주의 주기
-onChange를 바꿔야 할듯? 
-커링 함수 사용해서 onChange 사용해도 괜찮을듯?
-onChange(setState,e,Instructions,조건)
-조건
-닉네임: 빈칸이 아니여야함
-비밀번호: 비밀번호 형식에 맞아야함
-비밀번호 확인: 비밀번호와 같아야함 -> 커링함수로 가자
-
-조건은 css를 건들자.
-error: 인증번호 누를시 아래 버튼도 같이 눌림
-*/
--- src/utils/SignUpUtils/SignUpUtil.ts
+++ src/utils/SignUpUtils/SignUpUtil.ts
@@ -1,3 +1,5 @@
+import React from "react";
+
 const url = process.env.NEXT_PUBLIC_BASE_API
 
 export const checkEmail = (e:string) => {
@@ -7,8 +9,8 @@ export const checkEmail = (e:string) => {
 
 export const SendAuthentication = async (e:React.MouseEvent<HTMLElement>,email:string) => {
     e.preventDefault()
-    const check = await checkExistingEmail(email);
-    if(check)
+    const emailCheck = await checkExistingEmail(email);
+    if(emailCheck)
         submitEmail(email)
 }//email 인증번호 보내는 코드
 
@@ -30,20 +32,56 @@ const checkExistingEmail = async (email: string): Promise<boolean> => {
   };
 
 const submitEmail = async (email:string) => {
-    await fetch((`${url}/api/sign-up/email/${email}/verify`)) 
+    await fetch((`${url}/api/sign-up/email/${email}/verify`))
+    alert("인증번호가 이메일에 전송되었습니다.")
 }
 
-export const CheckAuthentication = async (email:string,emailAuth:string) => {
-    const check = await fetch((`${url}/api/sign-up/mail/${email}/code/${emailAuth}`)).then(res=>res).catch(e=>console.log(e))
+export const CheckAuthentication = async (e:React.MouseEvent<HTMLElement>,email:string,emailAuth:string) => {
+    e.preventDefault()
+    const authenticationCheck = await fetch((`${url}/api/sign-up/email/${email}/code/${emailAuth}`))
+    if(authenticationCheck){
+        alert("인증번호가 맞습니다.")
+    }
+    else{
+        alert("이메일 인증코드가 불일치합니다.")
+    }
 }
 
 
 export const CheckName = (name:string) => name===""
 
 export const CheckPassword = (password:string) => {
-    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-//유효성 체크
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;//유효성 체크
     return password!==""&&!(passwordRegex.test(password))
 }
 
 export const CheckPasswordCheck = (password:string, passwordCheck:string) => passwordCheck!==""&&password!==passwordCheck;
+
+
+
+export const submitSignUp = async (e:React.MouseEvent<HTMLElement>,email:string,emailCertification:string,name:string,password:string,passwordCheck:string) => {
+    e.preventDefault();
+    const postData = {
+        "code": emailCertification,
+        "name": name,
+        "email": email,
+        "password": password
+    }
+    if((name!==""&&/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(password)&&password===passwordCheck)){
+        let SignUpcheck = await fetch((`${url}/api/sign-up/pol`),{
+            method:'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(postData)
+        })
+        if(SignUpcheck.ok)
+            alert("회원가입에 성공하셨습니다.")
+        else{
+            console.log(SignUpcheck)
+        }
+    }
+    else{
+        console.log("형식이 맞지 않습니다.",!CheckName(name),CheckPassword(password),CheckPasswordCheck(password,passwordCheck))
+    }
+}
