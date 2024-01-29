e1478b1ab2df19269e810bfb11697efa091cd0f8
KYUTAE PARK <rbxo0109@gmail.com>
2024-01-29 23:56:55 +0900

    feat: 로그인 코드 수정

--- src/utils/loginUtils/LoginUtil.ts
+++ src/utils/loginUtils/LoginUtil.ts
@@ -2,19 +2,15 @@ const url =  process.env.NEXT_PUBLIC_BASE_API;
 
 export const signInLocally = async (e:React.FormEvent<HTMLFormElement>,id:string,password:string) => {
     e.preventDefault();
-    const signPost = {
-        code:"",
-        name:"",
-        email:id,
-        password:password
-    }
     const AccessToken = await fetch(`${url}/api/sign-in/pol`,
     {
         method:'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        credentials: 'include',
-        body: JSON.stringify(signPost)});
-    console.log(AccessToken);
+        body: JSON.stringify({        
+            email:id,
+            password:password
+        })});
+    console.log(AccessToken.json());
 }
