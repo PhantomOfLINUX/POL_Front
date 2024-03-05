b1839d03ea5fed102e0f51483ce15156af7e0a09
KYUTAE PARK <rbxo0109@gmail.com>
2024-03-05 23:09:50 +0900

    fet: middleware를 통한 엑세스 토큰 재발행

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,37 +1,38 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+
 const url =  process.env.NEXT_PUBLIC_BASE_API;
 
-
-const ReaccessToken = async (refreshToken:string) => {
-  const IsRefreshToken = await fetch(`${url}/api/auth/refresh`,{
-    method: "POST",
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body:JSON.stringify({refreshToken}),
-    credentials: 'include'
-  })
-  console.log(IsRefreshToken.status)
+const ReAccessToken = async (refreshToken:string) => {
+  try{
+    const newAccessTokenOk = await fetch(`${url}/api/auth/refresh`,{
+      credentials: 'include',
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({refreshToken}),
+    })
+    if(!newAccessTokenOk.ok){
+      alert("")
+    }
+  }catch(error) {
+    console.error(error)
+  }
 }
 
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   const { nextUrl, cookies } = request;
-  const { origin, pathname } = nextUrl;
+  const { pathname } = nextUrl;
   const accessToken = cookies.get("POL_ACCESS_TOKEN");
   const refreshToken = cookies.get("POL_REFRESH_TOKEN")
-  if(accessToken===undefined&&refreshToken!==undefined){ 
-    ReaccessToken(refreshToken.value)
-  }
-  if(pathname.startsWith("/problem")&&accessToken===undefined){
+  if(accessToken===undefined&&refreshToken!==undefined)
+    await ReAccessToken(refreshToken.value)
+  if(pathname.startsWith("/problem")&&accessToken===undefined)
     return NextResponse.redirect(new URL('/login', request.url))
-  }
 }
 
 export const config = {
   matcher: '/:path*',
 }
-
-//로그인유무 확인
