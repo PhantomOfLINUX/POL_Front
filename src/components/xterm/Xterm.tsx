ffadb109ef84e13836afffce08f34eda18105551
KYUTAE PARK <rbxo0109@gmail.com>
2024-03-06 02:18:37 +0900

    fix: wss 수정

--- src/components/xterm/Xterm.tsx
+++ src/components/xterm/Xterm.tsx
@@ -8,8 +8,7 @@ const socketUrl = process.env.NEXT_PUBLIC_WEBSOCKET_URL;
 
 const Xterm: React.FC = () => {
   const terminalRef = useRef<Terminal | null>(null);
-  const xtermContainerRef = useRef<HTMLDivElement | null>(null); // 터미널이 로드될 div의 ref
-
+  const xtermContainerRef = useRef<HTMLDivElement | null>(null);
   useEffect(() => {
     if (!terminalRef.current && xtermContainerRef.current) {
       const websocket = new WebSocket(socketUrl ? socketUrl : "");
