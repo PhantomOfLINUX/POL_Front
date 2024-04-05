00b7979bccd28d30620db0df9bb60e2d3df34c7a
KYUTAE PARK <rbxo0109@gmail.com>
2024-04-05 21:49:00 +0900

    hook 제어

--- src/hooks/useGetStages.tsx
+++ src/hooks/useGetStages.tsx
@@ -41,9 +41,10 @@ function useGetStages(
                 const isCompleted = searchParams.get("isCompleted")?.split(",")
                 const difficultyLevels = searchParams.get("difficultyLevels")?.split(",")
                 const stageGroupTypes = searchParams.get("stageGroupTypes")?.split(",")
+                const page_index = searchParams.get("page")
                 const params = {
-                    page_index:"1",
-                    page_size:"1",
+                    page_index:page_index||"1",
+                    page_size:"10",
                 };
                 const queryString = new URLSearchParams(params)
                 difficultyLevels?.forEach(level=>{queryString.append("difficultyLevels",level)})
