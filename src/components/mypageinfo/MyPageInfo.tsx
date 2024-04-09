0cf411bcd99c0adeeb13bdc01fad0ffa162b568f
tracer12 <vendetta12@naver.com>
2024-04-09 17:24:24 +0900

    test

--- src/components/mypageinfo/MyPageInfo.tsx
+++ src/components/mypageinfo/MyPageInfo.tsx
@@ -5,9 +5,15 @@ import Link from "next/link";
 import MyPageProblemForm from "./MyPageProblem";
 import MyPageProfileForm from "./MyPageProfile";
 import MyPageUserSettingForm from "./MyPageUserSetting";
+import { useMemo } from 'react';
 
-const MyPageInfoForm = () => {
+interface MyPageInfoFormType {
+    myPageSelector: number;
+}
 
+
+const MyPageInfoForm: React.FC<MyPageInfoFormType> = ({ myPageSelector }) => {
+    console.log(myPageSelector)
     return (
         <div className="border-2 border-gray w-1/2 items-center p-3 bg-color-white rounded-lg mb-6">
             <div>
