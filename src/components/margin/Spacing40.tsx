0e9841d58ba84dc2785a91ec6b32bc3a0c43b65a
Jeong-Rae <kkwjdfo@gmail.com>
2024-04-26 21:14:01 +0900

    [Feat] Spacing 컴포넌트 추가

---
+++ src/components/margin/Spacing40.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+
+const Spacing40: React.FC = () => {
+    const style = {
+        height: "40px",
+        backgroundColor: "transparent",
+    };
+
+    return <div style={style}></div>;
+};
+
+export default Spacing40;
