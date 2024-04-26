6ceafc1c176b0a88a38ebbc23836f081aa42263b
Jeong-Rae <kkwjdfo@gmail.com>
2024-04-26 21:14:17 +0900

    [Feat] tailwind 색상 설정 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,46 +1,71 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        "thema-color": "#2D347F",
-        "inputBorder-color": "#90929E",
-        "vaild-color": "#E7322D",
-        "description-color": "#21214C",
-        "socialBorder-color": "#4E5968",
-        gray: "#90929E",
-        red: "#D1180B",
-        "SelectBorder-color": "#90929E",
-        "problemStageLi-color": "#D1D5DB",
-        "disabled-color": "#D9D9D9",
-        "OpaqueBackground-color": "rgba(0, 0, 0, 0.3)",
-      },
-      minHeight: {
-        "min-height": "25rem",
-      },
-      height: {
-        "social-height": "6.125rem",
-        "problemStage-height": "50rem",
-      },
-      width: {
-        "problemStage-width": "5.75rem",
-        "problemStage-isCompleted": "5.5rem",
-        "problemStage-title": "11.75rem",
-        "problemStage-info": "21.25rem",
-      },
-      borderRadius: {
-        "problemStage-selected-radius": "2px",
-      },
-      borderWidth: {
-        "problemStageLi-borderWidth": "1px",
-      },
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                blue: {
+                    300: "#7ca6f3",
+                    400: "#5089ef",
+                    500: "#246beb",
+                    600: "#1d56bc",
+                    700: "#16408d",
+                },
+                gray: {
+                    0: "#ffffff",
+                    100: "#f0f0f0",
+                    200: "#e4e4e4",
+                    300: "#dbdbdb",
+                    400: "#c6c6c6",
+                    500: "#8e8e8e",
+                    600: "#717171",
+                    700: "#555555",
+                    800: "#2d2d2d",
+                    900: "#1d1d1d",
+                    1000: "#000000",
+                },
+                danger: {
+                    50: "#FEECF0",
+                    100: "#FCD4DE",
+                    500: "#EB003B",
+                    600: "#D50136",
+                },
+                "thema-color": "#2D347F",
+                "inputBorder-color": "#90929E",
+                "vaild-color": "#E7322D",
+                "description-color": "#21214C",
+                "socialBorder-color": "#4E5968",
+                red: "#D1180B",
+                "SelectBorder-color": "#90929E",
+                "problemStageLi-color": "#D1D5DB",
+                "disabled-color": "#D9D9D9",
+                "OpaqueBackground-color": "rgba(0, 0, 0, 0.3)",
+            },
+            minHeight: {
+                "min-height": "25rem",
+            },
+            height: {
+                "social-height": "6.125rem",
+                "problemStage-height": "50rem",
+            },
+            width: {
+                "problemStage-width": "5.75rem",
+                "problemStage-isCompleted": "5.5rem",
+                "problemStage-title": "11.75rem",
+                "problemStage-info": "21.25rem",
+            },
+            borderRadius: {
+                "problemStage-selected-radius": "2px",
+            },
+            borderWidth: {
+                "problemStageLi-borderWidth": "1px",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
