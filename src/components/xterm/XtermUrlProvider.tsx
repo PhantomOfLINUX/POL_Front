ca5b18bd8950c2cbd0449568ade1a881a52af99f
KYUTAE PARK <rbxo0109@gmail.com>
2024-04-30 01:49:56 +0900

    feat: xterm padding 조정

--- src/components/xterm/XtermUrlProvider.tsx
+++ src/components/xterm/XtermUrlProvider.tsx
@@ -17,7 +17,7 @@ interface XtermUrlProviderType {
 
 const XtermUrlProvider: React.FC<XtermUrlProviderType> = ({xtermConnectUrl}) => {
     return (
-        <div className="flex">  
+        <div className="flex w-full h-full justify-evenly pt-20 z-10">  
             <XtermQuestion/>
             <Xterm url={xtermConnectUrl?.read()?.url} query={xtermConnectUrl?.read()?.query}/>
         </div>
