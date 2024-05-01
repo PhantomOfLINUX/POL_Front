7203e9b856f6ab140ed1d517a9fefba5a792183a
KYUTAE PARK <rbxo0109@gmail.com>
2024-05-01 22:22:19 +0900

    feat: QuestionDesciption 작성

--- src/components/xterm/XtermQuestion/XtermQuestion.tsx
+++ src/components/xterm/XtermQuestion/XtermQuestion.tsx
@@ -5,6 +5,7 @@ import React,{useState} from "react";
 import useGetQuestion from "@/hooks/useGetQuestion";
 
 import XtermQuestionStage from "./XtermQuestionStage";
+import XtermQuestionDescription from "./XtermQuestionDescription";
 
 interface XtermQuestionType {
     accessToken: string | undefined,
@@ -24,8 +25,9 @@ const XtermQuestion:React.FC<XtermQuestionType> = ({accessToken,refreshToken}) =
     const [question_index,setQusetion_index] = useState(1);
     const questionInfo = useGetQuestion(accessToken,refreshToken,question_index)
     return (
-        <div className="bg-white rounded-XtermQuestion-Radius z-10 h-XtermQuestion-height min-w-XtermQuestion-width">
-            <XtermQuestionStage total_questions={questionInfo?.questionCount||0} question_index={question_index}/>
+        <div className="bg-white flex-col items-center rounded-XtermQuestion-Radius z-10 h-XtermQuestion-height min-w-XtermQuestion-width">
+            <XtermQuestionStage total_questions={3} question_index={question_index}/>
+            <XtermQuestionDescription title={questionInfo?.title} desciption={questionInfo?.description}/>
         </div>
     )
 }
