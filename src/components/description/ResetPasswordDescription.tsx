ee2962d7b15652d3f537d03f582e8fa3c8fb33f0
Jeong-Rae <kkwjdfo@gmail.com>
2024-05-02 07:20:34 +0900

    refactor: 비밀번호 초기화 화면 기능수정, 디자인수정
    Signed-off-by: Jeong-Rae <kkwjdfo@gmail.com>

--- src/components/description/ResetPasswordDescription.tsx
+++ src/components/description/ResetPasswordDescription.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 
 
-
-const LoginSignupDescription = () => {
+const ResetPasswordDescription = () => {
     return (
-        <h1 className="w-screen text-center text-2xl mt-10 mb-10 ">
-            <span className="text-thema-color font-[1000] mb-20">비밀번호 </span>변경하기<br />
-
-            <span className="text-gray text-sm font-bold">POL에 사용하는 이메일을 입력해주세요</span><br />
-            <span className="text-gray text-sm font-bold"> 메일이 확인 된 뒤에 비밀번호를 변경하실 수 있어요<br />
-            </span>
-        </h1 >
+        <div className="w-screen flex flex-col text-center mt-10 mb-10 space-y-2">
+            <p className="text-gray-900 font-light text-4xl">
+                <span className="text-blue-600 font-bold mb-20">비밀번호</span> 변경하기<br/>
+            </p>
+            <div className="h-12"></div>
+            <p className="text-gray-700 font-light text-lg">새로 사용하실 비밀번호를 입력해주세요</p>
+            <p className="text-gray-700 font-light text-lg">영어 소문자와 숫자, 특수문자로 8글자 이상으로 만들어야 해요</p>
+        </div>
     )
 }
 
 
-export default LoginSignupDescription
+export default ResetPasswordDescription
