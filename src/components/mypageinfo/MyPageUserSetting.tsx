a16b498b9b99ca79c0459427996eb998a7be63dd
tracer12 <vendetta12@naver.com>
2024-05-02 00:40:53 +0900

    마이페이지 완성했습니다 (#124)
    * 히트맵 캘린더 수정
    
    * 마이페이지 통합
    
    * 마이페이지 디자인, 모달창 수정
    
    * test
    
    * 마이페이지 완성

--- src/components/mypageinfo/MyPageUserSetting.tsx
+++ src/components/mypageinfo/MyPageUserSetting.tsx
@@ -36,13 +36,12 @@ const MyPageUserSettingForm: React.FC<MyPageInfoType> = ({ accessToken, refreshT
                                 비밀번호 변경 후 변경된 비밀번호로 다시 로그인하셔야 사용이 가능합니다
                             </div>
                             <div className="inline-block pt-6 pr-5">
-                                <button type="button" className="w-36 h-8 bg-thema-color text-white rounded">
+                                <button type="button" className="w-36 h-8 bg-blue-500 text-white rounded">
                                     <Link href="/changepassword">비밀번호 변경</Link>
                                 </button>
                             </div>
                         </div>
                     </div>
-                    <br />
                     <br />
                     <br />
                     <div className="px-5 font-bold">
@@ -57,7 +56,7 @@ const MyPageUserSettingForm: React.FC<MyPageInfoType> = ({ accessToken, refreshT
                                 삭제 시 개인정보 및 개인 식별 정보가 삭제 되며 통계정보는 유지 됩니다.
                             </div>
                             <div className="pt-6 pr-5">
-                                <button type="button" className="w-36 h-8 bg-thema-color text-white rounded"
+                                <button type="button" className="w-36 h-8 bg-danger-600 text-white rounded"
                                     onClick={() => { toggleModal() }}>
                                     계정 삭제
                                 </button>
@@ -68,9 +67,9 @@ const MyPageUserSettingForm: React.FC<MyPageInfoType> = ({ accessToken, refreshT
             </div>
             {
                 modal &&
-                (<div className="fixed flex justify-center items-center w-full max-h-screen z-0 top-0 left-0 right-0 bottom-0 bg-gray-500 bg-opacity-30" onClick={(e) => {e.preventDefault(); toggleModal()}}>
+                (<div className="fixed flex justify-center items-center w-full max-h-screen z-0 top-0 left-0 right-0 bottom-0 bg-gray-500 bg-opacity-50" onClick={(e) => { e.preventDefault(); toggleModal() }}>
                     <div className="">
-                        <div id="alert-additional-content-2" className="w-full p-4 mb-4 text-red-800 border border-red-300 rounded-lg bg-danger-100 dark:bg-gray-800 dark:text-red-400 dark:border-red-800" role="alert">
+                        <div id="alert-additional-content-2" className="w-full p-4 mb-4 text-danger-600 border border-2 border-danger-800 rounded-lg bg-danger-200 dark:bg-gray-800 dark:text-danger-400 dark:border-danger-800" role="alert">
                             <div className="flex items-center">
                                 <svg className="flex-shrink-0 w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                                     <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
@@ -84,16 +83,16 @@ const MyPageUserSettingForm: React.FC<MyPageInfoType> = ({ accessToken, refreshT
                                 그래도 삭제 하시겠습니까?
                             </div>
                             <div className="flex justify-between">
-                                <button type="button" className="text-white bg-red-800 hover:bg-red-900 focus:ring-4 focus:outline-none 
+                                <button type="button" className="text-white bg-danger-600 hover:bg-danger-800 focus:ring-4 focus:outline-none 
                                 focus:ring-red-300 font-medium rounded-lg text-sm px-6 py-3 me-2 text-center 
                                 inline-flex items-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
                                     onClick={(e) => { DeletePlayer(e, userId, accessToken) }}
                                 >
                                     <Link href="/">계정 삭제</Link>
                                 </button>
-                                <button type="button" className="float-right text-red-800 bg-transparent border border-red-800 
-                                hover:bg-red-900 hover:text-white focus:ring-4 focus:outline-none 
-                                focus:ring-red-300 font-medium rounded-lg text-sm px-6 py-3 
+                                <button type="button" className="float-right text-white bg-danger-600 border border-red-800 
+                                hover:bg-danger-800 hover:text-white focus:ring-4 focus:outline-none 
+                                focus:ring-danger-300 font-medium rounded-lg text-sm px-6 py-3 
                                 text-center dark:hover:bg-red-600 dark:border-red-600 dark:text-red-500 
                                 dark:hover:text-white dark:focus:ring-red-800"
                                     onClick={() => toggleModal()}>
