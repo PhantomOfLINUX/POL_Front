73bbba799d5a014b03f707e964782040572ea335
KYUTAE PARK <rbxo0109@gmail.com>
2024-05-01 16:50:02 +0900

    feat: Xterm Question 준비

--- src/components/xterm/XtermQuestion/XtermQuestion.tsx
+++ src/components/xterm/XtermQuestion/XtermQuestion.tsx
@@ -9,15 +9,35 @@ interface XtermQuestionType {
     refreshToken: string | undefined,
 }
 
+const dummyData = {
+    answerType: "MULTIPLE_CHOICE",
+    description: "string",
+    index: 1,
+    options: ['string'],
+    questionId: "Q013-001-M",
+    title: "string1"
+}
+
 const XtermQuestion:React.FC<XtermQuestionType> = ({accessToken,refreshToken}) => {
     const [question_index,setQusetion_index] = useState(1);
     const questionInfo = useGetQuestion(accessToken,refreshToken,question_index)
     return (
         <div className="bg-white rounded-XtermQuestion-Radius z-10 h-XtermQuestion-height min-w-XtermQuestion-width">
-            asdasd
+            <p>{dummyData.title}</p>
+            <span>{dummyData.description}</span>
         </div>
     )
 }
 
 
 export default XtermQuestion
+
+/*
+answerType: "MULTIPLE_CHOICE"
+description: "string"
+index: 1
+options: ['string']
+questionId: "Q013-001-M"
+title: "string1"
+
+*/
