ec85b5ba51ed6fb086bdde081c6a2b8b3f5e7cb1
Jeong-Rae <kkwjdfo@gmail.com>
2024-05-05 08:34:06 +0900

    feat: danger alert 추가
    Signed-off-by: Jeong-Rae <kkwjdfo@gmail.com>

--- src/components/alert/DangerAlert.tsx
+++ src/components/alert/DangerAlert.tsx
@@ -7,10 +7,10 @@ interface AlertProps {
 
 const DangerAlert = ({ title, message, onClose }: AlertProps) => {
     return (
-        <div className="bg-danger-500 border-t-4 border-danger-500 rounded-b text-danger-600 px-4 py-3 shadow-md w-2/5 mt-10" role="alert">
+        <div className="bg-danger-200 border-t-4 border-danger-500 rounded-b text-gray-900 px-4 py-3 shadow-md w-2/5 mt-10" role="alert">
             <div className="flex">
                 <div className="py-1">
-                    <svg className="fill-current h-6 w-6 text-teal-500 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                    <svg className="fill-current h-6 w-6 text-danger-500 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                         <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/>
                     </svg>
                 </div>
