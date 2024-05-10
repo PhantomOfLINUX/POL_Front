7fd613f38b5403551c48c4e82dcf6c1e5cf5bbd8
KYUTAE PARK <rbxo0109@gmail.com>
2024-05-10 23:03:29 +0900

    fix: action 취소

--- src/components/problemStage/problemStageSelect/ProblemStageSelect.tsx
+++ src/components/problemStage/problemStageSelect/ProblemStageSelect.tsx
@@ -8,8 +8,6 @@ import ProblemStageData from "../ProblemStageData.json"
 
 import type { problemStageSelectType } from "@/types/problemStage";
 
-const asd
-
 const ProblemStageSelect:React.FC<{selectName:problemStageSelectType}> = ({selectName}) => {
     const [ulToggle,setUlToggle] = useState<boolean>(false);
     const {problemListKoName, problemListUl} = ProblemStageData[selectName];
